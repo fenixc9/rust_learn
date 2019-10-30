45d1de4c3f7d3c5dcbcda2a7069bab02fce65297
hao.liu <hao.liu@aimapnet.com>
2019-10-30 18:35:28 +0800

    更新 channel部分

--- test_concurrent/src/channel.rs
+++ test_concurrent/src/channel.rs
@@ -1,21 +1,8 @@
-# 并发
-并发跟其他语言区别看似很大，但是底层操作系统和硬件的实现其实不可能有太大区别。
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
 
-所以Rust的并发控制本质上还是和其他语言类似的。
-
-底层api控制就是基本的mutex，atomic。
-
-应用层就是和golang类似的channel，区别在于Rust的channel的生产者分为同步发送和异步发送。
-
-### thread
-Rust的线程对应OS线程，同步标准库。
-##### 创建线程
-调用`thread::spawn`函数创建线程
-
-### channel
-```rust
-#[test]
-fn t2() {
+pub fn run_sync_channel(){
     let (tx, rx) = mpsc::channel();
 
     thread::spawn(move || {
@@ -43,14 +30,7 @@ fn t2() {
     //Got: thread
 }
 
-```
-`mpsc::channel`这个形式构建出来的是异步Sender
-
-可以看到，上例在消费者阻塞的情况下，生产者，仍然能立刻将所有信息立刻发送完毕，打印出send finish
-
-```rust
-#[test]
-fn t3() {
+pub fn run_async_channel(){
     let (tx, rx) = mpsc::sync_channel(0);
 
     thread::spawn(move || {
@@ -77,11 +57,3 @@ fn t3() {
     //Got: thread
     //send finish
 }
-```
-`mpsc::sync_channel`这个形式构建出来的是同步Sender
-
-可以看到，在消费者被阻塞的情况下，生产者也被阻塞了。
-
-
-
-
--- test_concurrent/src/main.rs
+++ test_concurrent/src/main.rs
@@ -2,13 +2,18 @@ use std::thread;
 use std::time::Duration;
 use std::sync::mpsc;
 
+mod channel;
+
+fn main() {
+
+}
 #[test]
 fn t1() {
+    // 直接用static变量，不加任何同步措施
     static mut VAR: i32 = 1;
     for _ in 0..10 {
         thread::spawn(move || {
             for _ in 0..10 {
-                // 完全不同步
                 unsafe {
                     VAR += 1;
                 }
@@ -21,46 +26,10 @@ fn t1() {
 
 #[test]
 fn t2() {
-    let (tx, rx) = mpsc::channel();
-
-    thread::spawn(move || {
-        let vals = vec![
-            String::from("hi"),
-            String::from("from"),
-            String::from("the"),
-            String::from("thread"),
-        ];
-
-        for val in vals {
-            tx.send(val).unwrap();
-            thread::sleep(Duration::from_secs(1));
-        }
-    });
-
-    for received in rx {
-        println!("Got: {}", received);
-    }
+    channel::run_sync_channel()
 }
 
 #[test]
 fn t3() {
-    let (tx, rx) = mpsc::sync_channel(3);
-
-    thread::spawn(move || {
-        let vals = vec![
-            String::from("hi"),
-            String::from("from"),
-            String::from("the"),
-            String::from("thread"),
-        ];
-
-        for val in vals {
-            tx.send(val).unwrap();
-            thread::sleep(Duration::from_secs(1));
-        }
-    });
-
-    for received in rx {
-        println!("Got: {}", received);
-    }
+    channel::run_async_channel()
 }
